a4efc7a10d70f9b51206535be64ed3ce320b38d0
Chris Archibald <carchi8py@gmail.com>
2015-10-09 21:27:09 -0700

    Make sure no matter how the user changes the variables the date get changed.

--- Weight Logger/DataViewController.swift
+++ Weight Logger/DataViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class DataViewController: UIViewController {
     
-    var weight: Double?
-    var date: NSDate?
+    //When we change our property through code (and viewDidLoad didn't call we want to do this so we don't get stale data
+    var weight: Double? {
+        didSet{
+            if let weight = weight {
+                WeightTextField.text = "\(weight)"
+            }
+        }
+    }
+    var date: NSDate? {
+        didSet {
+            if let date = date {
+                datePicker.date = date
+            }
+        }
+    }
     
     @IBOutlet weak var WeightTextField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
